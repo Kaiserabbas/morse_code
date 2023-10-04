935da7a8a11b21b234c06ab7215ad5ba52bb961b
tajemouti <tajemouti@gmail.com>
2023-10-04 19:02:24 +0100

    Fix linters' errors

--- morse_code_decoder.rb
+++ morse_code_decoder.rb
@@ -1,25 +1,25 @@
 CODE = {
-  ".-" => "A", "-..." => "B", "-.-." => "C", "-.." => "D",
-  "." => "E", "..-." => "F", "--." => "G", "...." => "H",
-  ".." => "I", ".---" => "J", "-.-" => "K", ".-.." => "L",
-  "--" => "M", "-." => "N", "---" => "O", ".--." => "P",
-  "--.-" => "Q", ".-." => "R", "..." => "S", "-" => "T",
-  "..-" => "U", "...-" => "V", ".--" => "W", "-..-" => "X",
-  "-.--" => "Y", "--.." => "Z",
-  "-----" => "0", ".----" => "1", "..---" => "2", "...--" => "3",
-  "....-" => "4", "....." => "5", "-...." => "6", "--..." => "7",
-  "---.." => "8", "----." => "9"
-}
+  '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
+  '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+  '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
+  '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
+  '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+  '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
+  '-.--' => 'Y', '--..' => 'Z',
+  '-----' => '0', '.----' => '1', '..---' => '2', '...--' => '3',
+  '....-' => '4', '.....' => '5', '-....' => '6', '--...' => '7',
+  '---..' => '8', '----.' => '9'
+}.freeze
 
 def decode_char(morse)
   CODE[morse]
 end
 
 def decode_word(morse_word)
-  word_chars = morse_word.split(" ")
-  decoded_word = ""
+  word_chars = morse_word.split
+  decoded_word = ''
 
-  for char in word_chars
+  word_chars.each do |char|
     decoded_word += decode_char(char)
   end
 
@@ -27,21 +27,20 @@ def decode_word(morse_word)
 end
 
 def decode_message(message)
-  message_words = message.split("   ")
-  decoded_message = ""
+  message_words = message.split('   ')
+  decoded_message = ''
 
-  for word in message_words
-    decoded_message += decode_word(word) + " "
+  message_words.each do |word|
+    decoded_message += "#{decode_word(word)} "
   end
 
   decoded_message.strip
 end
 
-
-morse_message_example = "-- -.--   -. .- -- .   .. ...   - .- .--- . -- --- ..- - .."
+morse_message_example = '-- -.--   -. .- -- .   .. ...   - .- .--- . -- --- ..- - ..'
 decoded_message = decode_message(morse_message_example)
 puts decoded_message
 
-message_from_bottle = ".-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ..."
+message_from_bottle = '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
 decoded_message_from_bottle = decode_message(message_from_bottle)
 puts decoded_message_from_bottle
